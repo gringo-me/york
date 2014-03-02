8b1bbe0d837cea83ad3fc04c02ae9d0a4045200d
root <root@debian.(none)>
2014-03-03 00:04:59 +0100

    saving

--- fifo_spinlock/char_fifo_spinlock.h
+++ fifo_spinlock/char_fifo_spinlock.h
@@ -7,13 +7,18 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/syscall.h>
+
+#define SET_SMP_PRIO 351 
 
 pid_t gettid() { return syscall( __NR_gettid ); }
-typedef struct fifo_spinlock_chart {
-volatile pid_t  taskid;
+
+typedef struct fifo {
+volatile pid_t taskid;
 volatile unsigned char owner;
 volatile unsigned char next;
-volatile unsigned char prio;
+volatile char *prio;
+//volatile unsigned char prio;
 volatile unsigned char task_prio;
 cpu_set_t saved_cpumask;
 
@@ -25,40 +30,45 @@ void fifo_init_lock_char(fifo_spinlock_chart *lock){
 	lock->next = 0;
 }
 
-void fifo_set_spinlock_ceiling(fifo_spinlock_chart *lock, unsigned char prio){
+void fifo_set_spinlock_smpceiling(fifo_spinlock_chart *lock, char *smp_prio){
 
-	lock->prio = prio;
+	lock->prio = smp_prio;
+	//lock->prio = prio;
 }
 
 void fifo_spin_lock_char(fifo_spinlock_chart * lock)
 {
 	unsigned char ticket;
-	int policy;
-	int old_prio;	
+	int policy,cpu,pid,old_prio;	
 	struct sched_param param;
 	cpu_set_t mask, holder_mask;	
 	
+	printf("Entering function lock\n");
 	ticket = __sync_fetch_and_add(&lock->next,1);
 	pthread_getschedparam (pthread_self(), &policy, &param);
-	//printf("Ticket: %d prio %d\n",ticket,param.sched_priority);
-	
-	old_prio = param.sched_priority;	
-	param.sched_priority = lock->prio;	
-	//pthread_setschedprio(pthread_self(),param.sched_priority);
+	printf("Ticket: %d prio %d\n",ticket,param.sched_priority);
 
+	cpu = sched_getcpu();
+	pid = gettid();	
+	old_prio = param.sched_priority;
+	
+	//param.sched_priority = lock->prio;	
+	//pthread_setschedprio(pthread_self(),param.sched_priority);
+        syscall(SET_SMP_PRIO,pid,lock->prio);	
+	
+	sched_getparam (0,&param);
+	
+	printf("--Thread %d priority %d --\n",gettid(),param.sched_priority);	
+	
 	sched_getaffinity(0, sizeof(mask), &mask);
 
         //if there is a holder add our current cpu affinity
-	//printf("Thread %d What's the value of lock-taskid ? %d\n",gettid(),lock->taskid);	
+	printf("Thread %d What's the value of lock-taskid ? %d\n",gettid(),lock->taskid);	
 	if(lock->taskid > 0){
 	printf("Thread %d IN LOOP What's the value of lock-taskid ? %d\n",gettid(),lock->taskid);	
-	
 	   
-	   printf("Thread %d Current cpu0 %d \n",gettid(),CPU_ISSET(0,&mask));	
-           printf("Thread %d Current cpu1 %d \n",gettid(),CPU_ISSET(1,&mask));
-	   //CPU_ZERO(&mask);
 	   sched_getaffinity(lock->taskid,sizeof(mask), &holder_mask);
-	   CPU_SET(sched_getcpu(), &holder_mask);
+	   CPU_SET(cpu, &holder_mask);
            int ret = sched_setaffinity(lock->taskid, sizeof(mask), &holder_mask);
 	   if(0 > ret ){
 		printf("Thread %d sched_affinity error %d %s \n",gettid(),ret,strerror(errno));
@@ -83,11 +93,13 @@ void fifo_spin_unlock_char(fifo_spinlock_chart * lock)
 
 	cpu_set_t mask;	
 
-	//sleep(1);
+	pthread_getschedparam (pthread_self(), &policy, &param);
+	
+	//unlock
 	lock->taskid = -1;
 	sched_getaffinity(0, sizeof(mask), &mask);
-	printf("Thread %d unlock current cpu0 %d \n",gettid(),CPU_ISSET(0,&mask));	
-        printf("Thread %d unlock current cpu1 %d \n",gettid(),CPU_ISSET(1,&mask));
+	printf("Thread %d unlock current cpu0 %d prio %d\n",gettid(),CPU_ISSET(0,&mask),param.sched_priority);	
+        printf("Thread %d unlock current cpu1 %d prio %d\n",gettid(),CPU_ISSET(1,&mask),param.sched_priority);
 	printf("Thread %d currently executing spin_unlock on cpu %d \n\n",gettid(),sched_getcpu());	
 
 	//set back original affinity
@@ -96,6 +108,7 @@ void fifo_spin_unlock_char(fifo_spinlock_chart * lock)
 	
 	//set back normal set prio
 	pthread_setschedprio(pthread_self(),param.sched_priority);
+	//unlock
 	__sync_fetch_and_add(&lock->owner,1);
 }
 
--- fifo_spinlock/pthread_test_char.c
+++ fifo_spinlock/pthread_test_char.c
@@ -3,21 +3,22 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <errno.h>
-
 #include <sys/time.h>
 #include <sys/resource.h>
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <limits.h>
+#include <sched.h>
+#include <stdlib.h>
 
-#include <list>
-
+#include <errno.h>
+#include <stdarg.h>
 #include "char_fifo_spinlock.h"
 
 #define LOOPS 5
 
-using namespace std;
 
-list<int> the_list;
 fifo_spinlock_chart fifo_lock;
 
 pthread_spinlock_t spinlock;
@@ -27,77 +28,132 @@ pthread_mutex_t mutex;
 
 /*When unsigned char reaches MAX_Uchar it wraps up and starts from zero again*/
 
-
+static int mark_fd = -1;
+static __thread char buff[BUFSIZ+1];
 /******************************************/
 
+static char prio_per_cpu[4] = {119,118,118,117};
+
+static char buffer[BUFSIZ];
 int cpu = 0;
 
+static void perr(char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vsnprintf(buffer, BUFSIZ, fmt, ap);
+	va_end(ap);
+
+	perror(buffer);
+	fflush(stderr);
+	exit(-1);
+}
+
+static void setup_ftrace_marker(void)
+{
+	struct stat st;
+	char *files[] = {
+		"/sys/kernel/debug/tracing/trace_marker",
+		"/debug/tracing/trace_marker",
+		"/debugfs/tracing/trace_marker",
+	};
+	int ret;
+	int i;
+
+	for (i = 0; i < (sizeof(files) / sizeof(char *)); i++) {
+		ret = stat(files[i], &st);
+		if (ret >= 0)
+			goto found;
+	}
+	/* todo, check mounts system */
+	return;
+found:
+	mark_fd = open(files[i], O_WRONLY);
+}
+
+static void ftrace_write(const char *fmt, ...)
+{
+	va_list ap;
+	int n;
+
+	if (mark_fd < 0)
+		return;
+
+	va_start(ap, fmt);
+	n = vsnprintf(buff, BUFSIZ, fmt, ap);
+	va_end(ap);
+
+	write(mark_fd, buff, n);
+}
 void *consumer(void *ptr)
 {
-    int i;
-    int j = 0;
-     
-    printf("Consumer TID %lu\n", (unsigned long)gettid());
+	int i;
+	int j = 0;
+	long pid;
+	int cpu;
 
-    cpu_set_t cpumask;
-    CPU_ZERO(&cpumask);
-    CPU_SET(cpu, &cpumask); cpu++;
-    sched_setaffinity(0, sizeof(cpumask), &cpumask);
-	
-    while (1)
-    {
-	fifo_spin_lock_char(&fifo_lock);	
+	printf("Consumer TID %lu\n", (unsigned long)gettid());
 
-        if (the_list.empty())
-        {
+	cpu_set_t cpumask;
+	CPU_ZERO(&cpumask);
+	CPU_SET(cpu, &cpumask); cpu++;
+	sched_setaffinity(0, sizeof(cpumask), &cpumask);
 
-	fifo_spin_unlock_char(&fifo_lock);
-            break;
-        }
+	pid = gettid();
 
+	printf("Thread %d id started \n",pid);
+
+	for (i = 0; i < LOOPS; i++){
+		cpu = sched_getcpu();
+
+		fifo_spin_lock_char(&fifo_lock);	
+		//we force thread migration by changing cpu affinities
+		cpu++;	  
+		if (cpu > 3) cpu = 0;
+
+		CPU_ZERO(&cpumask);
+		CPU_SET(cpu, &cpumask); 
+		printf("MSS531:pid %d next cpu %d \n",pid,cpu);	
+
+		fifo_spin_unlock_char(&fifo_lock);       
+	}
 	printf("Thread %d currently executing in cpu %d \n", gettid(),sched_getcpu());
-	
-	j = 0;
-	while(j < 1000000) j++;
-
-        i = the_list.front();
-        the_list.pop_front();
-
-	fifo_spin_unlock_char(&fifo_lock);       
-    }
-
-    return NULL;
+	return NULL;
 }
 
 int main()
 {
     int i;
     pthread_t thr1, thr2;
+    pthread_t *threads;
     struct timeval tv1, tv2;
     struct sched_param param;
+    long *thread_pids;
+    int nr_tasks = 1;
 
     fifo_init_lock_char(&fifo_lock);
-    fifo_set_spinlock_ceiling(&fifo_lock, sched_get_priority_max(SCHED_FIFO)-1); 
-    param.sched_priority = 3;
+    fifo_set_spinlock_smpceiling(&fifo_lock, prio_per_cpu); 
+    param.sched_priority = 110;
     sched_setscheduler(0, SCHED_FIFO, &param);
     
     printf("sched_max %d\n",sched_get_priority_max(SCHED_FIFO));
 
-    // Creating the list content...
-    for (i = 0; i < LOOPS; i++)
-        the_list.push_back(i);
-
     // Measuring time before starting the threads...
     gettimeofday(&tv1, NULL);
-
 	
+    threads = (pthread_t*) malloc(sizeof(*threads) * nr_tasks);
+    thread_pids = (long*) malloc(sizeof(long) * nr_tasks);
+    
+    for (i=0; i < nr_tasks; i++) {
+	    if (pthread_create(&threads[i], NULL, consumer, NULL))
+		    perr("pthread_create");
+    }
 
-    pthread_create(&thr1, NULL, consumer, NULL);
-    pthread_create(&thr2, NULL, consumer, NULL);
 
-    pthread_join(thr1, NULL);
-    pthread_join(thr2, NULL);
 
+	for (i=0; i < nr_tasks; i++)
+		pthread_join(threads[i], (void**)&thread_pids[i]);
     // Measuring time after threads finished...
     gettimeofday(&tv2, NULL);
 
